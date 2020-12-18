46891628518dc490843f80c9e07f91c9d955bee1
Mahdi  Olamaei <exxzam@gmail.com>
2020-12-18 18:03:03 +0330

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import ttk
+from tkinter import *
+import threading
+import time
+import os
+
+
+root = Tk()
+root.geometry('1000x500')
+
+
+
+
+root.mainloop()
